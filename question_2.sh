9919fe1c6548690d73fbd40b03d8fff951d8f6b0
Alex Lo <alexlocheungyu@gmail.com.hk>
2020-10-15 15:43:47 +0800

    Change the directory name

--- question_2.sh
+++ question_2.sh
@@ -3,8 +3,8 @@
 for i in {1..100}
 do
  # your-unix-command-here
-	mkdir DD${i}
-	cd DD${i}
+	mkdir DDM${i}
+	cd DDM${i}
 	echo "nanoseconds since 1970-01-01 00:00:00 UTC:"> time_till_now.txt
 	date +%s%N >> time_till_now.txt
 	cd ..

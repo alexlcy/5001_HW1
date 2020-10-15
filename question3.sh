afd22597c91a88c95351f34175402892742ec22c
Alex Lo <alexlocheungyu@gmail.com.hk>
2020-10-15 15:46:27 +0800

    Add some guideline to question 3

--- question3.sh
+++ question3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Part 1 answer of question 3"
 grep 'blockID=\"[ig][0-9]*\"' blocklist.xml
 
+
+echo "Part 2 answer of question 3"
 grep 'id=\"[a-z][a-zA-Z0-9]*@[a-zA-Z0-9]*.[a-zA-Z0-9]*' blocklist.xml
